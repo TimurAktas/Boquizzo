95396b5a4142a9377c653e3979c87d1523fee32b
Timur Aktas <Timur.aktas-gonzalez@adesso-mobile.de>
2022-10-12 22:44:49 +0200

    base app

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { LoginScreen } from './src/screens/LoginScreen/LoginScreen';
+
+
+function App() {
+  return (
+      <LoginScreen />
+  );
+}
+
+export default App;
+
---
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -0,0 +1,15 @@
+import React, {} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+export const LoginScreen: React.FC = () => {
+    return (
+        <View>
+            <Text>Hund</Text>
+        </View>
+    );
+};
+
+const style = StyleSheet.create({
+    viewStyle: { flex: 1, alignItems: 'center', backgroundColor: 'rgba(204, 226, 244, 0.4)' },
+});
